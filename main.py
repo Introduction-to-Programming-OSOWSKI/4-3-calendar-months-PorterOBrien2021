5c06987e5a37e0a06eb34241cc34f77713879f98
PorterOBrien2021 <824085@mystma.org>
2021-02-18 15:37:08 +0000

    calender 2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calender(x):
+def calendar(x):
     for i in range (0, 12):
         if months[i] == x:
             return i + 1
     return x + " is not a month"
 
 months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+print(calendar("march"))
